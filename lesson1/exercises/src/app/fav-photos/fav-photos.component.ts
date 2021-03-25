95af724c55125e41577b1645ac8ecc10168fabab
madsur <dennissheets@outlook.com>
2021-03-25 12:06:53 -0500

    save before moving on

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
-
+  image1 = 'assets/img/1.jpg';
+  image2 = 'assets/img/2.jpg';
+  image3 = 'assets/img/3.jpg';
+  image4 = 'assets/img/4.jpg';
   constructor() { }
 
   ngOnInit() {
