958287e8c6e40b9c134aaace620695f14c4c6a4a
madsur <dennissheets@outlook.com>
2021-04-02 22:53:30 -0500

    upto buttons

--- lesson3/examples/src/app/skill-set/skill-set.component.ts
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -1,4 +1,6 @@
+import { Attribute } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
    selector: 'skill-set',
@@ -7,10 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillSetComponent implements OnInit {
    listHeading: string = 'Some Coding Skills I Know';
-   skills: string[] = ['Loops', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
-   alternateColor: string = 'black';
-   bulletType: string = 'A';
-   changeColor: boolean = true;
+   skills: string[] = ['Loopss', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
+   alternateColor: string = 'purple';
+   bulletType: string = 'a';
+   changeColor: boolean = false;
+  
 
    constructor() { }
 
