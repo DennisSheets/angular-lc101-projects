c56d81811b439f837641859388c8b6f1a4ccfb2d
Dennis Sheets <dennissheets@outlook.com>
2021-11-09 12:38:23 -0600

    added a form-text-box within the form component

---
+++ lesson1/codeAlong/src/app/form/form-text-box/form-text-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-text-box',
+  templateUrl: './form-text-box.component.html',
+  styleUrls: ['./form-text-box.component.css']
+})
+export class FormTextBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
